18325bda8115682074b41fdd3b547f2163d535ae
Dhruv Brahmbhatt <dhruvbrahm007@gmail.com>
2025-02-03 18:19:08 +0530

    change of db string

---
+++ api/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+})
