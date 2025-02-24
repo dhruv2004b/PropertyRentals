f01ad5aff3860298e4c0ab76c1ed5d5fbd69152b
Dhruv Brahmbhatt <dhruvbrahm007@gmail.com>
2025-02-24 22:07:49 +0530

    created a user update API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,44 @@
-export const test=(req,res)=>{
-    res.json({
-        message:"Hello api is working"
-    });
-}
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
+export const test = (req, res) => {
+  res.json({
+    message: "Hello api is working",
+  });
+};
+
+export const updateUser = async (req, res, next) => {
+  console.log("🔍 Checking user permissions...");
+  console.log("User ID from Token:", req.user.id);
+  console.log("User ID from Request Params:", req.params.id);
+
+  if (req.user.id !== req.params.id) {
+    console.log("🚨 Unauthorized: User ID does not match!");
+    return next(errorHandler(401, "You can only update your own Account !"));
+  }
+  try {
+    if(req.body.password) {
+      req.body.password= bcryptjs.hashSync(req.body.password,10);
+    }
+
+    const updatedUser= await User.findByIdAndUpdate(req.params.id,{
+      $set:{
+        username:req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: req.body.avatar,
+      }
+    },{new: true})
+
+    console.log("✅ User Updated Successfully:", updatedUser._id);
+    
+
+    const {password, ...rest} = updatedUser._doc;
+
+    res.status(200).json(rest);
+
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
+import cookieParser from 'cookie-parser';
+import cors from "cors";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -15,6 +16,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app= express();
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:3000", // Change this to match your frontend URL
+    credentials: true,  // allows cookies in request
+  }));
 const port= 3000;
 
 app.listen(port,()=>{
