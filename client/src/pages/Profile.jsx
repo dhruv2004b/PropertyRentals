691446c5c918eb10756c8f55ba6701db49a9978b
Dhruv Brahmbhatt <dhruvbrahm007@gmail.com>
2025-03-02 01:49:06 +0530

    complete upload image

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,54 @@
 import { useSelector,useDispatch } from "react-redux";
-import { useRef, useState } from "react";
+import { useRef, useState,useEffect } from "react";
 import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from "../redux/user/userSlice";
 import {Link} from "react-router-dom";
+import {getStorage,ref, uploadBytesResumable} from 'firebase/storage';
+import {app} from '../firebase'
+
+
 export default function Profile() {
+  
   const { currentUser ,loading,error} = useSelector((state) => state.user);
   const fileRef= useRef(null);
   const [formData, setFormData] = useState({});
   const dispatch= useDispatch();
   const [updateSuccess,setUpdateSuccess]= useState(false);
-  // console.log(formData);
+  const [file,setFile]= useState(undefined);
+  const [filePerc,setFilePerc]= useState(0);
+  console.log(filePerc);
+  console.log(file);
 
+  // useEffect(()=>{
+  //   if(file){
+  //     handleFileUpload(file);
+
+  //   }
+  // },[file]);
+
+  // const handleFileUpload=(file)=>{
+  //   const storage= getStorage(app);
+  //   const fileName=new Date().getTime() + file.name;
+  //   const storageRef =ref(storage,fileName);
+  //   const uploadTask= uploadBytesResumable(storageRef,file);
+
+  //   uploadTask.on('state_changed',
+  //     (snapshot)=>{
+  //       const progress = (snapshot.bytesTransferred/
+  //         snapshot.totalBytes) * 100;
+  //         setFilePerc(Math.round(progress));
+        
+  //     },
+  //   )
+
+
+  // }
+
+  // console.log(formData);
+// firebase Storage
+// allow read;
+// allow write: if
+//       request.resource.size < 2* 1024 * 1024 &&
+//       request.resource.contentType.matches('image/.*')
   const handleChange=(e)=>{
     setFormData({ ...formData, [e.target.id] : e.target.value });
 
@@ -90,7 +129,7 @@ export default function Profile() {
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="file" ref={fileRef} hidden accept="image/*"/>
+        <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*"/>
         <img onClick={()=>fileRef.current.click()}
           className="rounded-full h-24 w-24 object-cover hover:cursor-pointer self-center mt-2"
           src={currentUser.avatar}
