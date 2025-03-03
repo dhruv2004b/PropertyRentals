062dd0ecf033475b6cab73c762718453b538f32d
Dhruv Brahmbhatt <dhruvbrahm007@gmail.com>
2025-03-04 00:41:58 +0530

    Completed delete user listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,15 +1,14 @@
 import express from 'express';
-import { createListing, uploadImages } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, uploadImages } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 // Route for uploading Base64 images
 router.post('/upload', uploadImages);
-
-
-
 // Route for creating a listing
 router.post('/create', verifyToken, createListing);
+
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router;
