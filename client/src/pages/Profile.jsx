40e899f8f7a8c66378b54479164db0a1c8e9ef5f
Dhruv Brahmbhatt <dhruvbrahm007@gmail.com>
2025-03-03 22:38:12 +0530

    complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,43 +12,15 @@ export default function Profile() {
   const fileRef= useRef(null);
   const [formData, setFormData] = useState({});
   const dispatch= useDispatch();
+  const [showListingsError,setShowListingsError]= useState(false);
+  const [userListings,setUserListings]=useState([]);
+
   const [updateSuccess,setUpdateSuccess]= useState(false);
   const [file,setFile]= useState(undefined);
   const [filePerc,setFilePerc]= useState(0);
   console.log(filePerc);
   console.log(file);
 
-  // useEffect(()=>{
-  //   if(file){
-  //     handleFileUpload(file);
-
-  //   }
-  // },[file]);
-
-  // const handleFileUpload=(file)=>{
-  //   const storage= getStorage(app);
-  //   const fileName=new Date().getTime() + file.name;
-  //   const storageRef =ref(storage,fileName);
-  //   const uploadTask= uploadBytesResumable(storageRef,file);
-
-  //   uploadTask.on('state_changed',
-  //     (snapshot)=>{
-  //       const progress = (snapshot.bytesTransferred/
-  //         snapshot.totalBytes) * 100;
-  //         setFilePerc(Math.round(progress));
-        
-  //     },
-  //   )
-
-
-  // }
-
-  // console.log(formData);
-// firebase Storage
-// allow read;
-// allow write: if
-//       request.resource.size < 2* 1024 * 1024 &&
-//       request.resource.contentType.matches('image/.*')
   const handleChange=(e)=>{
     setFormData({ ...formData, [e.target.id] : e.target.value });
 
@@ -125,6 +97,26 @@ export default function Profile() {
 
     }
   }
+
+  const handleShowListings= async()=>{
+    try{
+      setShowListingsError(false);
+      const res = await fetch (`/api/user/listings/${currentUser._id}`);
+      const data= await res.json();
+
+      if(data.success===false)
+      {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+
+    }catch(error)
+    {
+      setShowListingsError(true);
+
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -171,6 +163,30 @@ export default function Profile() {
       </div>
       <p className="text-red-700 mt-5" >{error?error :' '}</p>
       <p className="text-green-700 mt-5" >{updateSuccess?'User is Updated Successfully' :' '}</p>
+
+      <button className="text-green-700 w-full" onClick={handleShowListings}>Show Listings</button>
+      <p className="text-red-700 mt-5">{showListingsError?'Error showing Listings':''}</p>
+
+      {userListings && userListings.length>0 && 
+      <div className="flex flex-col gap-4">
+        <h1 className="text-center mt-7 text-2xl font-semibold" >Your Listings</h1>
+        {userListings.map((listing)=>
+          <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+            <Link to={`/listing/${listing._id}`} >
+            <img src={listing.imageUrls[0]} alt="listing cover" className="h-16 w-16 object-contain "  />
+            </Link>
+            <Link className="text-slate-700 font-semibold hover:underline truncate flex-1"to={`/listing/${listing._id}`} >
+            <p >{listing.name}</p>
+            </Link>
+            <div className="flex flex-col items-center">
+              <button className="text-red-700 uppercase">Delete</button>
+              <button className="text-green-700 uppercase">Edit</button>
+            </div>
+            
+          </div>
+        )}
+      </div>
+      }
     </div>
   );
 }
