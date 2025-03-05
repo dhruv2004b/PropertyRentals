42a9d6fe4933e868005753095f0eb9087f1bdf0f
Dhruv Brahmbhatt <dhruvbrahm007@gmail.com>
2025-03-05 14:53:49 +0530

    create search aoi route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, getListing, updateListing, uploadImages } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, getListing, getListings, updateListing, uploadImages } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -14,4 +14,7 @@ router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id',verifyToken,updateListing);
 
 router.get('/get/:id',getListing);
+
+router.get('/get',getListings);
+
 export default router;
