769357ce9b02fc27238381bb44c6bcb7415df2f5
Dhruv Brahmbhatt <dhruvbrahm007@gmail.com>
2025-03-05 05:55:10 +0530

    added contact landlord functionality to the listing page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -18,44 +18,45 @@ export const updateUser = async (req, res, next) => {
   console.log("User ID from Request Params:", req.params.id);
 
   // Convert both to strings before comparing
-if (req.user.id.toString() !== req.params.id.toString()) {
-  console.log("🚨 Unauthorized: User ID does not match!");
-  return next(errorHandler(401, "You can only update your own Account !"));
-}
+  if (req.user.id.toString() !== req.params.id.toString()) {
+    console.log("🚨 Unauthorized: User ID does not match!");
+    return next(errorHandler(401, "You can only update your own Account !"));
+  }
   try {
-    if(req.body.password) {
-      req.body.password= bcryptjs.hashSync(req.body.password,10);
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
 
-    const updatedUser= await User.findByIdAndUpdate(req.params.id,{
-      $set:{
-        username:req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        avatar: req.body.avatar,
-      }
-    },{new: true})
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
 
     console.log("✅ User Updated Successfully:", updatedUser._id);
-    
 
-    const {password, ...rest} = updatedUser._doc;
+    const { password, ...rest } = updatedUser._doc;
 
     res.status(200).json(rest);
-
   } catch (error) {
     next(error);
   }
 };
 
-
 // ==================================================DELETE USER===============================================================
 
-export const deleteUser=async(req,res,next)=>{
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your own account !!"));
 
-  if(req.user.id!== req.params.id) return next(errorHandler(401,'You can only delete your own account !!'))
-
-  try{
+  try {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token", {
       httpOnly: true,
@@ -68,37 +69,40 @@ export const deleteUser=async(req,res,next)=>{
       console.log("❌ User not found!");
       return next(errorHandler(404, "User not found!"));
     }
-    res.status(200).json('User has been Deleted !!');
-
-  }catch(error)
-  {
-    next(error)
+    res.status(200).json("User has been Deleted !!");
+  } catch (error) {
+    next(error);
   }
-  
-  
-
 };
 
 // =====================================================GET USER LISTINGS=========================================================
 
-export const getUserListings=async(req,res,next)=>{
-
-  
-  if(req.user.id===req.params.id)
-    
-    {
-      try{
-        const listings= await Listing.find({userRef:req.params.id});
-        res.status(200).json(listings);
-
-    }catch(error)
-    {
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
       next(error);
-  
     }
-      
-    }else{
-      return next(errorHandler(401,'You can only view your own listings'));
+  } else {
+    return next(errorHandler(401, "You can only view your own listings"));
+  }
+};
+
+
+// ==================================================GET USER======================================================================
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return next(errorHandler(404, "User not Found !!"));
     }
 
-}
+    const { password: pass, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import {Swiper,SwiperSlide} from 'swiper/react';
 import SwiperCore  from 'swiper';
+import { useSelector } from 'react-redux';
 import {Navigation}  from 'swiper/modules';
 import 'swiper/css/bundle';
 import {
@@ -13,6 +14,7 @@ import {
     FaParking,
     FaShare,
   } from 'react-icons/fa';
+import Contact from '../components/Contact';
   
   // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
@@ -23,6 +25,8 @@ export default function Listing() {
     const [loading,setLoading]= useState(false);
     const [error,setError]= useState(false);
     const [copied, setCopied] = useState(false);
+    const [contact,setContact]= useState(false);
+    const {currentUser}= useSelector((state)=>state.user);
     const params= useParams();
     useEffect(()=>{
         const fetchListing= async()=>{
@@ -134,6 +138,11 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact &&(
+
+            <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>Contact Landlord</button>
+            )}
+            {contact && <Contact listing={listing}/>}
           </div>
                 </div>
             )
